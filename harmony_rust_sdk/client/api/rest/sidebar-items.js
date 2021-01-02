99b36354070b74c4560d98ccc93babe8ea76f56a
yusdacra <yusdacra@users.noreply.github.com>
2021-01-02 22:17:49 +0000

    deploy: 83cded128a1f23c1c4d933089ae400585f602ea0

---
+++ harmony_rust_sdk/client/api/rest/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["FileId","A \"file id\", which can either be a HMC URL or a plain ID string."]],"fn":[["download","Downloads a file using a file ID."],["upload","Uploads a file to the homeserver."]]});
--- harmony_rust_sdk/client/api/sidebar-items.js
+++ harmony_rust_sdk/client/api/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["auth","Auth service client API."],["chat","Chat service client API."],["mediaproxy","Media proxy service client API."]]});
+initSidebarItems({"enum":[["HmcParseError","Errors that can occur when converting a possibly non-HMC URL to a HMC URL."]],"mod":[["auth","Auth service client API."],["chat","Chat service client API."],["mediaproxy","Media proxy service client API."],["rest","REST client API."]],"struct":[["Hmc","A HMC."]]});
