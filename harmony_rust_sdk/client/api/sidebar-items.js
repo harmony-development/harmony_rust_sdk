9bd908ee459cb0ec22d36215557a3d41752f6feb
yusdacra <yusdacra@users.noreply.github.com>
2021-01-13 22:48:09 +0000

    deploy: eb15281e7c0baaef90824dd7b731d0cc3ef38880

--- harmony_rust_sdk/client/api/sidebar-items.js
+++ harmony_rust_sdk/client/api/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["auth","Auth service client API."],["chat","Chat service client API."],["mediaproxy","Media proxy service client API."],["rest","REST client API."]]});
+initSidebarItems({"mod":[["auth","Auth service client API."],["chat","Chat service client API."],["mediaproxy","Media proxy service client API."],["rest","REST client API."]],"trait":[["ClientRequest",""]]});
