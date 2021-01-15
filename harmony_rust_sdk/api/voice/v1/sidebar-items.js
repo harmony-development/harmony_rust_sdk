e6ff8fce61f4449f6fa34835a98164420b8e7856
yusdacra <yusdacra@users.noreply.github.com>
2021-01-15 19:10:42 +0000

    deploy: 6c931b3555bb9fbf7c36b8b22b0e576735e7b934

--- harmony_rust_sdk/api/voice/v1/sidebar-items.js
+++ harmony_rust_sdk/api/voice/v1/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["client_signal",""],["signal",""],["voice_service_client","Generated client implementations."],["voice_service_server","Generated server implementations."]],"struct":[["ClientSignal",""],["Signal",""]]});
+initSidebarItems({"mod":[["client_signal","Nested message and enum types in `ClientSignal`."],["signal","Nested message and enum types in `Signal`."],["voice_service_client","Generated client implementations."],["voice_service_server","Generated server implementations."]],"struct":[["ClientSignal",""],["Signal",""]]});
