298db484e2a9d97de65bd999135745587cc72870
yusdacra <yusdacra@users.noreply.github.com>
2021-01-19 16:39:32 +0000

    deploy: b34069b8688d0abe5696c5624176d7ecb601136f

--- implementors/core/iter/traits/collect/trait.IntoIterator.js
+++ implementors/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["harmony_rust_sdk"] = [{"text":"impl IntoIterator for Hmcs","synthetic":false,"types":[]}];
+implementors["harmony_rust_sdk"] = [{"text":"impl IntoIterator for FileIds","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
