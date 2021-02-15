57d5aeffd91345d1d6f53efefb9b6f35ec7ae21b
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-02-15 18:27:34 +0300

    chore(deps): update flake deps

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -1,14 +1,26 @@
 { common }:
 with common; with pkgs;
 devshell.mkShell {
-  packages =
-    [ git nixpkgs-fmt rustc cachix binutils ]
-    ++ crateDeps.nativeBuildInputs ++ crateDeps.buildInputs;
-  env = {
-    NIX_CONFIG = ''
-      substituters = https://cache.nixos.org https://harmony-rust-sdk.cachix.org
-      trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= harmony-rust-sdk.cachix.org-1:m6dligIAIzpfXY3Z3hqYLLMSCaf+fd5QklxRGN1bEUg=
-    '';
-    LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${lib.makeLibraryPath neededLibs}";
-  } // env;
+  packages = [ rustc binutils ] ++ crateDeps.nativeBuildInputs ++ crateDeps.buildInputs;
+  commands =
+    let
+      pkgCmd = pkg: { package = pkg; };
+    in
+    [
+      (pkgCmd git)
+      (pkgCmd nixpkgs-fmt)
+      (pkgCmd cachix)
+    ];
+  env =
+    let
+      nv = lib.nameValuePair;
+    in
+    [
+      (
+        nv "NIX_CONFIG" ''
+          substituters = https://cache.nixos.org https://harmony-rust-sdk.cachix.org
+          trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= harmony-rust-sdk.cachix.org-1:m6dligIAIzpfXY3Z3hqYLLMSCaf+fd5QklxRGN1bEUg=
+        ''
+      )
+    ] ++ env;
 }
