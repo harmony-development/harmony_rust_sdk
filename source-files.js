1b63e91f5f9071fbce250444f0dd7ab2205a470c
yusdacra <yusdacra@users.noreply.github.com>
2021-06-11 22:20:43 +0000

    deploy: 3f1ae23494f291b465b0aeda5f4c88f6ed82e55d

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["harmony_rust_sdk"] = {"name":"","dirs":[{"name":"api","files":["chat.rs","mod.rs","rest.rs"]},{"name":"client","dirs":[{"name":"api","dirs":[{"name":"chat","files":["channel.rs","emote.rs","guild.rs","invite.rs","message.rs","mod.rs","permissions.rs","profile.rs"]}],"files":["auth.rs","mediaproxy.rs","mod.rs","rest.rs"]}],"files":["error.rs","mod.rs"]}],"files":["lib.rs"]};
+sourcesIndex["harmony_rust_sdk"] = {"name":"","dirs":[{"name":"api","files":["chat.rs","harmonytypes.rs","mod.rs","rest.rs"]},{"name":"client","dirs":[{"name":"api","dirs":[{"name":"chat","files":["channel.rs","emote.rs","guild.rs","invite.rs","message.rs","mod.rs","permissions.rs","profile.rs"]}],"files":["auth.rs","mediaproxy.rs","mod.rs","rest.rs"]}],"files":["error.rs","mod.rs"]}],"files":["lib.rs"]};
 createSourceSidebar();
