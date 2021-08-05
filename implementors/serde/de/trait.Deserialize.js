e1eb8ce1e7633fa5f1ff36cdad5a998fd4026513
yusdacra <yusdacra@users.noreply.github.com>
2021-08-05 15:33:44 +0000

    deploy: eb4e00a55fae4fc615c5df401059d59f9763d6aa

---
+++ implementors/serde/de/trait.Deserialize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["harmony_rust_sdk"] = [{"text":"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.127/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"harmony_rust_sdk/api/rest/struct.About.html\" title=\"struct harmony_rust_sdk::api::rest::About\">About</a>","synthetic":false,"types":["harmony_rust_sdk::api::rest::About"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
