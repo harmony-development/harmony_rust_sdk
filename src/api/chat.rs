ccb7d7e8ef2b9d5012d51ee498a445bceeb7af17
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-09-11 22:16:54 +0300

    tests(chat): add color decode / encode tests

--- src/api/chat.rs
+++ src/api/chat.rs
@@ -264,19 +264,38 @@ impl Display for InviteId {
 
 /// Functions for working with color in Harmony API.
 pub mod color {
-    pub fn encode_rgb(color: [u8; 3]) -> i32 {
+    /// Encode an RGB value.
+    pub fn encode_rgb(color: impl Into<[u8; 3]>) -> i32 {
+        let color = color.into();
         let mut c = color[0] as i32;
         c = (c << 8) + color[1] as i32;
         c = (c << 8) + color[2] as i32;
         c as i32
     }
 
-    pub fn decode_rgb(color: i32) -> [u8; 3] {
+    /// Decode an RGB value.
+    pub fn decode_rgb(color: impl Into<i32>) -> [u8; 3] {
+        let color = color.into();
         [
             ((color >> 16) & 255) as u8,
             ((color >> 8) & 255) as u8,
             (color & 255) as u8,
         ]
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn encode() {
+            assert_eq!(encode_rgb([0, 0, 0]), 0);
+        }
+
+        #[test]
+        fn decode() {
+            assert_eq!(decode_rgb(0), [0, 0, 0]);
+        }
     }
 }
 
