4bc9246d65a2222cc899f981bf3fad3b2423dcfa
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-11-29 22:03:53 +0300

    docs: reformat readme and lib readme

--- README.md
+++ README.md
@@ -5,19 +5,27 @@
 ![MSRV](https://img.shields.io/badge/MSRV-current%20stable-red)
 
 Rust implementation of [the Harmony chat protocol](https://github.com/harmony-development).
-Currently implements a lightweight client and a client API (powered by [hrpc](https://crates.io/crates/hrpc)), along with auto generated API via [hrpc-build](https://crates.io/crates/hrpc-build).
+Currently implements a lightweight client and a client API (powered by [hrpc](https://crates.io/crates/hrpc)),
+along with auto generated API via [hrpc-build](https://crates.io/crates/hrpc-build).
 
 ## Requirements
+
 - Latest stable Rust and Cargo.
-- If you are using Nix, `nix-shell` (or `nix develop` if you use flakes) should get you covered.
-- Otherwise, you'll need to get protobuf and make sure `protoc` is in your `PATH` env variable.
-  - If for some reason `build.rs` fails, make sure to set: 
+- If you are using Nix, `nix-shell` (or `nix develop` if you use flakes) should
+get you covered.
+- Otherwise, you'll need to get protobuf and make sure `protoc` is in your `PATH`
+env variable.
+  - If for some reason `build.rs` fails, make sure to set:
     - `PROTOC` env variable to your `protoc` executable
-    - and `PROTOC_INCLUDE` env variable to wherever protobuf include files are located, most likely in `/usr/share/include`.
+    - and `PROTOC_INCLUDE` env variable to wherever protobuf include files are
+    located, most likely in `/usr/share/include`.
 
 ## Examples
-- `echo_bot`: Showcases a simple message echo bot that operates in a guild. It will repost messages whenever someone else posts a message.
-- `message_log`: Showcases a simple message log bot that operates in a guild. It will log messages to the console whenever someone posts a message.
+
+- `echo_bot`: Showcases a simple message echo bot that operates in a guild.
+It will repost messages whenever someone else posts a message.
+- `message_log`: Showcases a simple message log bot that operates in a guild.
+It will log messages to the console whenever someone posts a message.
 - `cmd_bot`: A more complex bot that responds to "commands". The commands are:
   - r!ping: responds with "Pong! Took X secs."
   - r!hello: responds with "Hello, username!"
@@ -27,9 +35,13 @@ Currently implements a lightweight client and a client API (powered by [hrpc](ht
   - Make sure the bot has necessary permissions to view channels / send messages etc.
 
 ## Crate features
-- By default, only a bare-bones common API of all services is generated. You can customize the crate to your needs by enabling feature(s) listed below:
-  - Enable the `client_native` feature for a lightweight client implementation that uses `hyper` and works on native platforms.
-  - Enable the `client_web` feature for a lightweight client implementation that works on web platforms (WASM).
+
+- By default, only a bare-bones common API of all services is generated. You can
+customize the crate to your needs by enabling feature(s) listed below:
+  - Enable the `client_native` feature for a lightweight client implementation
+  that uses `hyper` and works on native platforms.
+  - Enable the `client_web` feature for a lightweight client implementation that
+  works on web platforms (WASM).
   - Enable the `gen_client` feature to generate client service code.
   - Enable the `gen_server` feature to generate server service code.
   - (Default) Enable the `gen_chat` feature to generate chat service code.
@@ -43,6 +55,7 @@ Currently implements a lightweight client and a client API (powered by [hrpc](ht
   - (Default) Enable the `gen_batch` feature to generate batch service code.
 
 ## MSRV
+
 Minimum Supported Rust Version: current stable.
 
 Changing MSRV is not considered a semver-breaking change.
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,50 @@
 #![recursion_limit = "256"]
 
 /*!
+![GitHub Workflow Status](https://img.shields.io/github/workflow/status/yusdacra/harmony_rust_sdk/Rust)
+[![crates.io](https://img.shields.io/crates/v/harmony_rust_sdk)](https://crates.io/crates/harmony_rust_sdk)
+[![docs.rs](https://docs.rs/harmony_rust_sdk/badge.svg)](https://docs.rs/harmony_rust_sdk)
+[![docs.rs](https://img.shields.io/badge/docs-master-blue)](https://harmonyapp.io/harmony_rust_sdk)
+![MSRV](https://img.shields.io/badge/MSRV-current%20stable-red)
+
 Rust implementation of [the Harmony chat protocol](https://github.com/harmony-development).
-Currently implements a lightweight client and a client API (powered by [hrpc](https://crates.io/crates/hrpc)), along with auto generated API via [hrpc-build](https://crates.io/crates/hrpc-build).
+Currently implements a lightweight client and a client API (powered by [hrpc](https://crates.io/crates/hrpc)),
+along with auto generated API via [hrpc-build](https://crates.io/crates/hrpc-build).
 
 ## Requirements
+
 - Latest stable Rust and Cargo.
-- If you are using Nix, `nix-shell` (or `nix develop` if you use flakes) should get you covered.
-- Otherwise, you'll need to get protobuf and make sure `protoc` is in your `PATH` env variable.
+- If you are using Nix, `nix-shell` (or `nix develop` if you use flakes) should
+get you covered.
+- Otherwise, you'll need to get protobuf and make sure `protoc` is in your `PATH`
+env variable.
   - If for some reason `build.rs` fails, make sure to set:
     - `PROTOC` env variable to your `protoc` executable
-    - and `PROTOC_INCLUDE` env variable to wherever protobuf include files are located, most likely in `/usr/share/include`.
+    - and `PROTOC_INCLUDE` env variable to wherever protobuf include files are
+    located, most likely in `/usr/share/include`.
 
 ## Examples
-- `echo_bot`: Showcases a simple message echo bot that operates in a guild. It will repost messages whenever someone else posts a message.
-- `message_log`: Showcases a simple message log bot that operates in a guild. It will log messages to the console whenever someone posts a message.
+
+- `echo_bot`: Showcases a simple message echo bot that operates in a guild.
+It will repost messages whenever someone else posts a message.
+- `message_log`: Showcases a simple message log bot that operates in a guild.
+It will log messages to the console whenever someone posts a message.
 - `cmd_bot`: A more complex bot that responds to "commands". The commands are:
   - r!ping: responds with "Pong! Took X secs."
   - r!hello: responds with "Hello, username!"
   - r!uptime: responds with "Been running for X secs."
-
 - Bot run instructions:
   - Run bots with `GUILD_INVITE=invite cargo run --example example_name`.
   - Make sure the bot has necessary permissions to view channels / send messages etc.
 
 ## Crate features
-- By default, only a bare-bones common API of all services is generated. You can customize the crate to your needs by enabling feature(s) listed below:
-  - Enable the `client` feature for a lightweight client implementation and the client API.
+
+- By default, only a bare-bones common API of all services is generated. You can
+customize the crate to your needs by enabling feature(s) listed below:
+  - Enable the `client_native` feature for a lightweight client implementation
+  that uses `hyper` and works on native platforms.
+  - Enable the `client_web` feature for a lightweight client implementation that
+  works on web platforms (WASM).
   - Enable the `gen_client` feature to generate client service code.
   - Enable the `gen_server` feature to generate server service code.
   - (Default) Enable the `gen_chat` feature to generate chat service code.
@@ -40,9 +58,11 @@ Currently implements a lightweight client and a client API (powered by [hrpc](ht
   - (Default) Enable the `gen_batch` feature to generate batch service code.
 
 ## MSRV
-Minimum Supported Rust Version: previous stable.
+
+Minimum Supported Rust Version: current stable.
 
 Changing MSRV is not considered a semver-breaking change.
+
 !*/
 
 /// Harmony protocol API.
