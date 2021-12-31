ac07835b87b5d3f2e2190ad0a23fa4bc3577aeae
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-12-31 19:02:06 +0300

    build(nix): add override for rust sdk

--- flake.nix
+++ flake.nix
@@ -10,5 +10,18 @@
   outputs = inputs: inputs.nixCargoIntegration.lib.makeOutputs {
     root = ./.;
     buildPlatform = "crate2nix";
+    overrides = {
+      crateOverrides = common: _: {
+        harmony-rust-sdk = prev: {
+          src = common.pkgs.fetchFromGitHub {
+            owner = "harmony-development";
+            repo = "harmony_rust_sdk";
+            rev = inputs.self.rev;
+            sha256 = common.pkgs.lib.fakeHash;
+            fetchSubmodules = true;
+          };
+        };
+      };
+    };
   };
 }
